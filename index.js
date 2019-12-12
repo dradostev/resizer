f0ae8c519c2d6f20e36e5d7b1ce9b3e151f649f7
Daniil Radostev <d.a.radostev@gmail.com>
2019-12-12 16:31:34 +0300

    to console

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
 const uniqid = require("uniqid");
+const argv = require('yargs').array('images').parse();
+const handler = require("./handler");
 
-const {
-  handler
-} = require("./handler");
 
-app.use(bodyParser.json());
+const task = {
+  uid: uniqid(),
+  assets: [{
+    src: "https://upsaleslab-users.s3.eu-central-1.amazonaws.com/Nature.zip",
+    dest: "./downloads/Nature.zip",
+    type: "zip"
+  }]
+}
 
-app.get("/health", (request, response) => {
-  response.statusCode = 200;
-  response.send();
-});
+const options = {
+  logger: console,
+  skipCleanUp: false
+}
 
-app.post("/run", (request, response) => {
-
-  const task = {
-    uid: uniqid(),
-    assets: [{
-      src: "https://upsaleslab-users.s3.eu-central-1.amazonaws.com/Nature.zip",
-      dest: "Nature.zip",
-      type: "zip"
-    }]
-  }
-
-  const options = {
-    logger: console,
-    skipCleanUp: false
-  }
-
-  const res = handler(task, options)
-    .then(job => {
-      console.log("> successfully finished.");
-    })
-    .catch(err => {
-      console.error("> failed.");
-      console.error(err);
-    });
-
-  response.send();
-});
-
-app.listen(5000);
+const res = handler(task, options)
+  .then(job => {
+    console.log("> successfully finished.");
+  })
+  .catch(err => {
+    console.error("> failed.");
+    console.error(err);
+  });
