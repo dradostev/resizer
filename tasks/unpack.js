c618b8a0147325c649b56f340bfab5171ab95ce0
Daniil Radostev <d.a.radostev@gmail.com>
2019-12-12 17:13:28 +0300

    unzip

--- tasks/unpack.js
+++ tasks/unpack.js
@@ -1,7 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const AdmZip = require('adm-zip')
+
 module.exports = (task, options) => {
   return new Promise((resolve, reject) => {
     try {
       options.logger.log(`[${task.uid}]: unpacking.`)
+
+      glob(path.join(__dirname, '/../downloads/*.zip'), null, (err, files) => {
+        if (err) {
+          options.logger.error(err)
+          return
+        }
+        files.forEach(file => {
+          const zip = new AdmZip(file)
+          zip.extractAllTo(path.join(__dirname, '/../downloads/'))
+        })
+      })
+
       resolve(task)
     } catch (err) {
       reject(err)
